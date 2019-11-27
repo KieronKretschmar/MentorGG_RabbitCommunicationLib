2a680cc72ebc4ed9af9b8b380da9c2201c59839f
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2019-11-27 14:28:48 +0100

    Add hash transfer model

--- TransferModels/BaseTransferModel.cs
+++ TransferModels/BaseTransferModel.cs
@@ -1,18 +1,18 @@
 ﻿using Newtonsoft.Json;
 
 public class BaseTransferModel
-    {
-        public int DemoId { get; set; }
+{
+    public int DemoId { get; set; }
 
-        public string ToJSON()
-        {
-            var json = JsonConvert.SerializeObject(this);
-            return json;
-        }
+    public string ToJSON()
+    {
+        var json = JsonConvert.SerializeObject(this);
+        return json;
+    }
 
     public static implicit operator byte[](BaseTransferModel operand)
     {
         var model = operand.ToJSON();
         return System.Text.Encoding.UTF8.GetBytes(model);
     }
-    }
+}
---
+++ TransferModels/HashTransferModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class HashTransferModel : BaseTransferModel
+{
+    public bool isDuplicate { get; set; }
+}
+
