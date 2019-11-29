d50f1a5dca7623983d5459da4ed35c15501a3657
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2019-11-29 16:53:57 +0100

    add general gatherer transfer model

---
+++ TransferModels/GathererTransferModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class GathererTransferModel : BaseTransferModel
+{
+    public string DownloadUrl { get; set; }
+    public DateTime MatchDate { get; set; }
+    public long UploaderId { get; set; }
+    public byte Source { get; set; }
+    public byte UploadType { get; set; }
+
+}
