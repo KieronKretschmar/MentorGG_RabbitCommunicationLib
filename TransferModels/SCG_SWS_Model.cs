7487c4129b68a9a45e60eacba1d47abe97d90adb
Kieron <kieron.kretschmar@outlook.com>
2019-12-18 10:27:31 +0100

    add TransferModels/SCG_SWS_Model.cs for SharingCodeGatherer to SteamWorksConnection comms

---
+++ TransferModels/SCG_SWS_Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace RabbitTransfer.TransferModels
+{    public class SCG_SWS_Model : TransferModel
+    {
+        public long UploaderId { get; set; }
+        public string SharingCode { get; set; }
+        public byte UploadType { get; set; }
+    }
+}
+
