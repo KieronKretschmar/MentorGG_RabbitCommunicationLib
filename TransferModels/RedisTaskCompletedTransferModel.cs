faee010f40ebf66493812b564e82a77ea2e85531
Kieron <kieron@mentor.gg>
2020-01-10 19:19:33 +0100

    Add RedisTaskCompletedTransferModel

---
+++ TransferModels/RedisTaskCompletedTransferModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitTransfer.TransferModels
+{
+    public class RedisTaskCompletedTransferModel : TaskCompletedTransferModel
+    {
+        public string RedisKey { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
