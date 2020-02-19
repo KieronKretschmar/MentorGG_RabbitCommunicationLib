17ad7db6b5dd22d7dbfd12c51d6bcdf47bd14aed
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-02-19 16:49:05 +0100

    Add new bool for BlobDownload success; remove zippedFilePath field

--- TransferModels/DemoAnalyzeReport.cs
+++ TransferModels/DemoAnalyzeReport.cs
@@ -3,12 +3,12 @@
 
     public class DemoAnalyzeReport : TaskCompletedReport
     {
+        public bool BlobDownloadFailed { get; set; } = false;
         public bool Unzipped { get; set; } = false;
         public bool DuplicateChecked { get; set; } = false;
         public bool IsDuplicate { get; set; } = false;
         public bool UploadedToRedis { get; set; } = false;
         public int FramesPerSecond { get; set; }
-        public string zippedFilePath { get; set; }
         public string Hash { get; set; }
     }
 }
