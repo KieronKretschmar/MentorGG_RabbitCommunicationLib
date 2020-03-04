be9464cf6ce9348ca5d8dacf75baae2262196a6c
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-03-04 15:06:57 +0100

    Create new interface for MatchId

---
+++ TransferModels/Interfaces/IMatchId.cs
@@ -0,0 +1,10 @@
+﻿namespace RabbitCommunicationLib.TransferModels.Interfaces
+{ 
+  interface IMatchId
+    {
+        /// <summary>
+        /// Unique Id to a Demo
+        /// </summary>
+        long MatchId{ get; set; }
+    }
+}
