c4ce6dd7e4bdd989e028196b7930dd01c5717f47
Kieron <kieron@mentor.gg>
2020-03-09 12:15:54 +0100

    Format and comments

--- Enums/ConsumedMessageHandling.cs
+++ Enums/ConsumedMessageHandling.cs
@@ -6,8 +6,19 @@ namespace RabbitCommunicationLib.Enums
 {
     public enum ConsumedMessageHandling
     {
-        Done=1,
-        Resend=2,
-        ThrowAway =3,
+        /// <summary>
+        /// When the message was (succesfully) consumed on no additional action needs to be performed.
+        /// </summary>
+        Done = 1,
+
+        /// <summary>
+        /// When the message needs to be resent, e.g. because it could not be consumed succesfully because of temporary reasons, e.g. a dependency being unavailable.
+        /// </summary>
+        Resend = 2,
+
+        /// <summary>
+        /// When the message could not be handled, and it should not be tried again.
+        /// </summary>
+        ThrowAway = 3,
     }
 }
