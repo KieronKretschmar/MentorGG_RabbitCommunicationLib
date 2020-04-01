16887022090fe925c4ba03369d2b1a842099964e
Kieron <kieron@mentor.gg>
2020-04-01 10:58:51 +0200

    Add ManualDownloadReport.cs

---
+++ TransferModels/ManualDownloadReport.cs
@@ -0,0 +1,38 @@
+﻿using RabbitCommunicationLib.Enums;
+using RabbitCommunicationLib.TransferModels.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitCommunicationLib.TransferModels
+{
+    public class ManualDownloadReport : TransferModel
+    {
+        public string BlobUrl { get; set; }
+
+        /// <summary>
+        /// When the Match occured.
+        /// </summary>
+        public DateTime UploadDate { get; set; }
+
+        /// <summary>
+        /// When the Match occured.
+        /// </summary>
+        public DateTime MatchDate { get; set; }
+
+        /// <summary>
+        /// The SteamId of the Uploader.
+        /// </summary>
+        public long UploaderId { get; set; }
+
+        /// <summary>
+        /// Where the Demo came from.
+        /// </summary>
+        public Source Source { get; set; }
+
+        /// <summary>
+        /// The method used to Upload.
+        /// </summary>
+        public UploadType UploadType { get; set; }
+    }
+}
