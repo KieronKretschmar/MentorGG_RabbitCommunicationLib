3fca1ae8f0ea40fa18624b73cb4d9d9fd0003c9a
Arran <code@arran.nz>
2020-05-18 15:28:24 +0200

    Migrate DemoAnalzeReport to block enum

--- TransferModels/DemoAnalyzeReport.cs
+++ TransferModels/DemoAnalyzeReport.cs
@@ -1,4 +1,5 @@
-﻿using RabbitCommunicationLib.TransferModels.Interfaces;
+﻿using RabbitCommunicationLib.Enums;
+using RabbitCommunicationLib.TransferModels.Interfaces;
 using System;
 
 namespace RabbitCommunicationLib.TransferModels
@@ -9,7 +10,7 @@ namespace RabbitCommunicationLib.TransferModels
         /// <summary>
         /// If not null, state of which the analyzation failed.
         /// </summary>
-        public DemoAnalyzeFailure Failure { get; set; }
+        public DemoAnalysisBlock Block { get; set; }
 
         /// <summary>
         /// Redis key to locate the resource
@@ -30,53 +31,5 @@ namespace RabbitCommunicationLib.TransferModels
         /// Unique identifier of the Demo.
         /// </summary>
         public string Hash { get; set; }
-    }
-
-    /// <summary>
-    /// Possible failures when Analzing a Demo.
-    /// </summary>
-    public enum DemoAnalyzeFailure
-    {
-
-        /// <summary>
-        /// Unknown Failure.
-        /// </summary>
-        Unknown = 0,
-        
-        /// <summary>
-        /// Indicates the Blob Download failed.
-        /// </summary>
-        BlobDownload = 1,
-
-        /// <summary>
-        /// Indicates Unzipping Failed.
-        /// </summary>
-        Unzip = 2,
-
-        /// <summary>
-        /// Indicates Http Communication for checking the Demo Hash failed.
-        /// </summary>
-        HttpHashCheck = 3,
-
-        /// <summary>
-        /// Indicates the Demo was a duplicate.
-        /// </summary>
-        Duplicate = 4,
-
-        /// <summary>
-        /// Indicates the Analyze step failed.
-        /// </summary>
-        Analyze = 5,
-
-        /// <summary>
-        /// Indicates the Enrich step failed.
-        /// </summary>
-        Enrich = 6,
-
-        /// <summary>
-        /// Indicates Storage to Redis failed.
-        /// </summary>
-        RedisStorage = 7,
-        
     }
 }
