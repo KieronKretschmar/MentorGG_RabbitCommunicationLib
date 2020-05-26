75fab72dc388cc77d9881ce7ebd134a085d0364a
Kieron <kieron@mentor.gg>
2020-05-26 16:23:39 +0200

    Add MatchDatabaseInsertionReport

---
+++ TransferModels/MatchDatabaseInsertionReport.cs
@@ -0,0 +1,26 @@
+﻿using RabbitCommunicationLib.Enums;
+using RabbitCommunicationLib.TransferModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitCommunicationLib.TransferModels
+{
+    public class MatchDatabaseInsertionReport : TaskCompletedReport
+    {
+        /// <summary>
+        /// The step during which the database upload failed, or null if it succeeded.
+        /// </summary>
+        public DemoAnalysisBlock? Block { get; set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="matchId"></param>
+        public MatchDatabaseInsertionReport(long matchId) : base(matchId)
+        {
+            Block = null;
+        }
+    }
+}
