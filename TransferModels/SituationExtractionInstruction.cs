78c8118d71ec653468c7738e9e6b669812861345
Kieron <kieron@mentor.gg>
2020-05-26 17:56:49 +0200

    Add SituationExtractionInstruction

---
+++ TransferModels/SituationExtractionInstruction.cs
@@ -0,0 +1,26 @@
+﻿using RabbitCommunicationLib.TransferModels.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitCommunicationLib.TransferModels
+{
+    /// <summary>
+    /// Instructions for SituationOperator to extract situations and upload them to the SituationDb.
+    /// </summary>
+    public class SituationExtractionInstruction : TransferModel, IMatchId
+    {
+        /// <inheritdoc/>
+        public long MatchId { get; set; }
+
+        /// <summary>
+        /// Redis key to locate the resource
+        /// </summary>
+        public string RedisKey { get; set; }
+
+        /// <summary>
+        /// Date of expiry for this resource
+        /// </summary>
+        public DateTime ExpiryDate { get; set; }
+    }
+}
