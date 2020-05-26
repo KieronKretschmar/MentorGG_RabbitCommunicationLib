d514387bf1b2429fa79931a35562e74e32e6dba8
Kieron <kieron@mentor.gg>
2020-05-26 15:11:27 +0200

    Add SituationOperatorResponseModel

---
+++ TransferModels/SituationOperatorResponseModel.cs
@@ -0,0 +1,34 @@
+﻿using RabbitCommunicationLib.Enums;
+using RabbitCommunicationLib.TransferModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitCommunicationLib.TransferModels
+{
+    public class SituationOperatorResponseModel : TransferModel
+    {
+        /// <summary>
+        /// Id of the Match which was attempted to be analyzed by SituationOperator.
+        /// </summary>
+        public long MatchId { get; set; }
+
+        /// <summary>
+        /// The outcome of the attemped analysis.
+        /// </summary>
+        public DemoAnalysisBlock Block { get; set; }
+
+        /// <summary>
+        /// Indicates if the download was succsesful
+        /// </summary>
+        public bool Success { get; set; }
+
+        public SituationOperatorResponseModel(long matchId)
+        {
+            MatchId = matchId;
+            Block = DemoAnalysisBlock.UnknownSituationOperator;
+            Success = false;
+        }
+    }
+}
